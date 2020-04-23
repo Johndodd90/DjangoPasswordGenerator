8dcffcfa9b060cb7fe13469317de5a738f20e302
John Dodd <johndodd90@gmail.com>
2020-04-23 16:51:14 +0100

    first commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\john\desktop\python specialist path\django\nick walter\django3 udemy course\project1 passwordgenerator\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
